37c06f8338fc99ca842aeca952a16d0fa47de83f
ryoxu1217 <ryousukexiaogu@gmail.com>
2026-09-20 17:14:13 +0900

    a

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -1,16 +1,40 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
 
-public sealed class GameManager : MonoBehaviour
+public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
 
-    void Awake()
+    [SerializeField] private Image fadeImage;
+    [SerializeField] private float fadeDuration = 1f;
+
+    private void Awake()
     {
         Instance = this;
     }
 
     public void OnPlayerDead()
     {
-        Debug.Log("Game Over");
+        StartCoroutine(GameOverSequence());
+    }
+
+    private IEnumerator GameOverSequence()
+    {
+        // フェードアウト
+        float t = 0;
+        Color c = fadeImage.color;
+
+        while (t < fadeDuration)
+        {
+            t += Time.deltaTime;
+            c.a = Mathf.Lerp(0, 1, t / fadeDuration);
+            fadeImage.color = c;
+            yield return null;
+        }
+
+        // ゲームオーバー画面へ
+        SceneManager.LoadScene("GAMEOVER");
     }
 }
